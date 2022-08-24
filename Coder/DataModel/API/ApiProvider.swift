1ae2fc3406fbf7bf99748d1ba2c2c3648f26a8db
NikitaRekaev <nvvvvk@gmail.com>
2022-08-24 14:36:03 +0200

    feature 2.4/ Added ApiProvider

---
+++ Coder/DataModel/API/ApiProvider.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+class ApiProvider {
+    init(baseUrl: URL = URL(string: "https://stoplight.io/mocks/")!) {
+        self.baseUrl = baseUrl
+    }
+    private let baseUrl: URL
+    @discardableResult func getData<Response: Codable>(
+        _ model: Response.Type = Response.self,
+        from endpoint: String,
+        _ completion: @escaping (Result<Response, Error>) -> Void
+    ) -> URLSessionDataTask {
+        let url = baseUrl.appendingPathComponent(endpoint)
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Ошибка получения данных")
+                DispatchQueue.main.async {
+                    if let error = error {
+                    completion(.failure(error))
+                    } else {
+                        print("неизвестная ошибка")
+                    }
+                }
+             return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd"
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                let result = try decoder.decode(Response.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    print("Не получается сконвертировать JSON: \(error)")
+                    completion(.failure(error))
+                }
+            }
+        }
+        dataTask.resume()
+        return dataTask
+    }
+}
