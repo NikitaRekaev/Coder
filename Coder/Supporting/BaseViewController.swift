e6d1b1e087609e50f4a75dbcc8bfc48976640edf
NikitaRekaev <nvvvvk@gmail.com>
2022-08-24 15:12:33 +0200

    feature 3.3/ Disabled force_cast in BaseViewController

--- Coder/Supporting/BaseViewController.swift
+++ Coder/Supporting/BaseViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class BaseViewController<T: UIView>: UIViewController {
+    // swiftlint:disable force_cast
     var mainView: T { view as! T }
+    // swiftlint:enable
     override func loadView() {
             view = T()
     }
