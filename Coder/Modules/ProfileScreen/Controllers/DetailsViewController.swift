56ceb88d79c29af4c5572c2546eaeaa2807e06e7
NikitaRekaev <nvvvvk@gmail.com>
2022-08-25 11:29:33 +0200

    fix2/ Added correct display of age in the profile

--- Coder/Modules/ProfileScreen/Controllers/DetailsViewController.swift
+++ Coder/Modules/ProfileScreen/Controllers/DetailsViewController.swift
@@ -41,13 +41,21 @@ class DetailsViewController: BaseViewController<ProfileView> {
     }
     func calculateYears(date: Date?) -> String {
         if let date = date {
-
+            
             let calendar = Calendar.current
             let dateCurrent = Date()
-
+            
             if let years = calendar.dateComponents([.year], from: date, to: dateCurrent).year {
-                let str = "\(years)"
-            return str
+                var stringOfAge = "\(years)"
+                let arrayOfAge = stringOfAge.compactMap{$0.wholeNumberValue}
+                if arrayOfAge.last != nil {
+                    switch arrayOfAge.last! {
+                    case 1: stringOfAge = "\(years) год"
+                    case 2...4: stringOfAge = "\(years) года"
+                    default: stringOfAge = "\(years) лет"
+                    }
+                }
+                return stringOfAge
             }
         }
         return "Не удалось вычислить год"
