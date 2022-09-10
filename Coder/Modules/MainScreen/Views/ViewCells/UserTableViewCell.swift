a8411812c74ff36b45d2cf5e4f53292b66bed641
NikitaRekaev <nvvvvk@gmail.com>
2022-09-10 16:10:02 +0200

    Added skeletons in UserTableViewCell

--- Coder/Modules/MainScreen/Views/ViewCells/UserTableViewCell.swift
+++ Coder/Modules/MainScreen/Views/ViewCells/UserTableViewCell.swift
@@ -47,6 +47,34 @@ class UserTableViewCell: UITableViewCell {
         return view
     }()
     
+    //  MARK: SkeletonView
+    
+    private let nameSkeletonView: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 144, height: 16)
+        view.layer.cornerRadius = 8
+        view.backgroundColor = UIColor(red: 0.955, green: 0.955, blue: 0.965, alpha: 1)
+        return view
+    }()
+    
+    private let departmentSkeletonView: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 80, height: 12)
+        view.layer.cornerRadius = 6
+        view.backgroundColor = UIColor(red: 0.955, green: 0.955, blue: 0.965, alpha: 1)
+        return view
+    }()
+    
+    private let imageSkeletonView: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 72, height: 72)
+        view.layer.cornerRadius = 36
+        view.backgroundColor = UIColor(red: 0.955, green: 0.955, blue: 0.965, alpha: 1)
+        return view
+    }()
+    
+    // MARK: - Initialization
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .white
@@ -57,18 +85,65 @@ class UserTableViewCell: UITableViewCell {
         addSubview(departmentLabel)
         addSubview(birthdayLabel)
         
+        addSubview(imageSkeletonView)
+        addSubview(nameSkeletonView)
+        addSubview(departmentSkeletonView)
+        
         setupConstraints()
+        setSkeletonViewsConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Public method
+    
     func setBirthdayLabelVisibility(shouldShowBirthday: Bool) {
         birthdayLabel.isHidden = !shouldShowBirthday
     }
     
+    func setLoadingView() {
+        
+        nameLabel.isHidden = true
+        avatarImageView.isHidden = true
+        departmentLabel.isHidden = true
+        tagLabel.isHidden = true
+        departmentLabel.isHidden = true
+        
+        imageSkeletonView.isHidden = false
+        nameSkeletonView.isHidden = false
+        departmentSkeletonView.isHidden = false
+        
+    }
+    
+    func setViewWithData() {
+        
+        nameLabel.isHidden = false
+        avatarImageView.isHidden = false
+        departmentLabel.isHidden = false
+        tagLabel.isHidden = false
+        departmentLabel.isHidden = false
+        
+        imageSkeletonView.isHidden = true
+        nameSkeletonView.isHidden = true
+        departmentSkeletonView.isHidden = true
+        
+    }
+    
+    func setData(firstName: String, lastName: String, tag: String, department: Department?, dateBirth: String) {
+        
+        avatarImageView.image = UIImage(named: "goose")
+        nameLabel.text = "\(firstName) \(lastName)"
+        tagLabel.text = tag
+        departmentLabel.text = department?.title
+        birthdayLabel.text = dateBirth
+    }
+    
+    // MARK: - Private methods
+    
     private func setupConstraints() {
+
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             avatarImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
@@ -96,11 +171,30 @@ class UserTableViewCell: UITableViewCell {
         ])
     }
     
-    func setData(firstName: String, lastName: String, tag: String, department: Department?, dateBirth: String) {
-        avatarImageView.image = UIImage(named: "goose")
-        nameLabel.text = "\(firstName) \(lastName)"
-        tagLabel.text = tag
-        departmentLabel.text = department?.title
-        birthdayLabel.text = dateBirth
+    private func setSkeletonViewsConstraints() {
+
+        imageSkeletonView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageSkeletonView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageSkeletonView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            imageSkeletonView.heightAnchor.constraint(equalToConstant: 72),
+            imageSkeletonView.widthAnchor.constraint(equalToConstant: 72)
+        ])
+        
+        nameSkeletonView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nameSkeletonView.leadingAnchor.constraint(equalTo: imageSkeletonView.trailingAnchor, constant: 16),
+            nameSkeletonView.centerYAnchor.constraint(equalTo: imageSkeletonView.centerYAnchor, constant: -20),
+            nameSkeletonView.widthAnchor.constraint(equalToConstant: 144),
+            nameSkeletonView.heightAnchor.constraint(equalToConstant: 16)
+        ])
+        
+        departmentSkeletonView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            departmentSkeletonView.leadingAnchor.constraint(equalTo: nameSkeletonView.leadingAnchor),
+            departmentSkeletonView.centerYAnchor.constraint(equalTo: nameSkeletonView.centerYAnchor, constant: 20),
+            departmentSkeletonView.widthAnchor.constraint(equalToConstant: 80),
+            departmentSkeletonView.heightAnchor.constraint(equalToConstant: 12)
+        ])
     }
 }
