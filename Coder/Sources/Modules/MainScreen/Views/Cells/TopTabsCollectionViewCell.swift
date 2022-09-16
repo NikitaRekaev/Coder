3497a3d4f2b0a737336197466ba765b0b61b180e
NikitaRekaev <nvvvvk@gmail.com>
2022-09-16 23:43:49 +0200

    Added constants for TopTabsCollectionViewCell

--- Coder/Sources/Modules/MainScreen/Views/Cells/TopTabsCollectionViewCell.swift
+++ Coder/Sources/Modules/MainScreen/Views/Cells/TopTabsCollectionViewCell.swift
@@ -2,36 +2,51 @@ import UIKit
 
 final class TopTabsCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - Constants
+    
     static let identifier = "Cell"
+    
+    private enum Constants {
+        static let textFont = R.Fonts.interMedium(with: 15)
+        static let textSelectedFont = R.Fonts.interSemiBold(with: 15)
+        static let borderHeight: CGFloat = 2
+        static let contentHeight: CGFloat = 36
+        static let contentWidth: CGFloat = -16
+        static let contentLeading: CGFloat = 16
+    }
+    
+    // MARK: - Properties
     
     private(set) var model: Department?
     
-    private let bottomBorderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 2))
+    // MARK: - Views
+    
+    private let bottomBorderView = UIView()
     private let label = UILabel()
     
     func setCellSelected(_ isSelected: Bool) {
         if isSelected {
             bottomBorderView.isHidden = false
-            label.textColor = UIColor(red: 0.02, green: 0.02, blue: 0.063, alpha: 1)
-            label.font = UIFont(name: "Inter-Medium", size: 15)
+            label.textColor = R.Colors.Text.active
+            label.font = Constants.textFont
         } else {
             bottomBorderView.isHidden = true
-            label.textColor = UIColor(red: 0.591, green: 0.591, blue: 0.609, alpha: 1)
-            label.font = UIFont(name: "Inter-SamiBold", size: 15)
+            label.textColor = R.Colors.Text.inActive
+            label.font = Constants.textSelectedFont
         }
     }
     
     func setModel(_ department: Department) {
         self.model = department
         label.text = department.title
-        label.textColor = UIColor(red: 0.591, green: 0.591, blue: 0.609, alpha: 1)
+        label.textColor = R.Colors.Text.inActive
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         label.textColor = .black
         
-        bottomBorderView.backgroundColor = #colorLiteral(red: 0.3960784314, green: 0.2039215686, blue: 1, alpha: 1)
+        bottomBorderView.backgroundColor = R.Colors.violet
         bottomBorderView.isHidden = false
         
         contentView.addSubview(label)
@@ -58,15 +73,15 @@ final class TopTabsCollectionViewCell: UICollectionViewCell {
             bottomBorderView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             bottomBorderView.leadingAnchor.constraint(equalTo: label.leadingAnchor),
             bottomBorderView.trailingAnchor.constraint(equalTo: label.trailingAnchor),
-            bottomBorderView.heightAnchor.constraint(equalToConstant: 2)
+            bottomBorderView.heightAnchor.constraint(equalToConstant: Constants.borderHeight)
         ])
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            contentView.heightAnchor.constraint(equalToConstant: 36),
-            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            contentView.heightAnchor.constraint(equalToConstant: Constants.contentHeight),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.contentLeading),
             contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            contentView.widthAnchor.constraint(equalTo: widthAnchor, constant: -16)
+            contentView.widthAnchor.constraint(equalTo: widthAnchor, constant: Constants.contentWidth)
         ])
     }
 }
