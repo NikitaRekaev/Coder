b0e7896e5ff8abe1fdde1e1c55405d399404830f
NikitaRekaev <nvvvvk@gmail.com>
2022-11-04 19:08:13 +0600

    Updated SortView

--- Coder/Sources/Modules/SortScreen/Views/SortView.swift
+++ Coder/Sources/Modules/SortScreen/Views/SortView.swift
@@ -5,8 +5,9 @@ final class SortView: BaseView {
     // MARK: - Views
     
     lazy var backButton = UIButton()
-    lazy var alphabetSortButton = UIButton()
-    lazy var birthdaySortButton = UIButton()
+    lazy var alphabetSortButton = SortButton()
+    lazy var birthdaySortButton = SortButton()
+    lazy var buttonStack = UIStackView()
     
     private lazy var titleLabel = UILabel()
     
@@ -31,23 +32,16 @@ private extension SortView {
         
         backButton.setImage(R.Images.backArrow, for: .normal)
         
-        alphabetSortButton.setTitle(Constants.AlphabetSortButton.text, for: .normal)
-        alphabetSortButton.setImage(R.Images.Sort.unChecked, for: .normal)
-        alphabetSortButton.setImage(R.Images.Sort.isChecked, for: .selected)
-        alphabetSortButton.setTitleColor(R.Colors.Text.active, for: .normal)
-        alphabetSortButton.titleLabel?.font = Constants.SortButton.font
-        alphabetSortButton.titleEdgeInsets.left = Constants.SortButton.insetsLeft
+        buttonStack.axis = .vertical
+        buttonStack.spacing = 35
+        buttonStack.alignment = .leading
         
+        alphabetSortButton.setTitle(Constants.AlphabetSortButton.text, for: .normal)
         birthdaySortButton.setTitle(Constants.BirthdaySortButton.text, for: .normal)
-        birthdaySortButton.setImage(R.Images.Sort.unChecked, for: .normal)
-        birthdaySortButton.setImage(R.Images.Sort.isChecked, for: .selected)
-        birthdaySortButton.setTitleColor(R.Colors.Text.active, for: .normal)
-        birthdaySortButton.titleLabel?.font = Constants.SortButton.font
-        birthdaySortButton.titleEdgeInsets.left = Constants.SortButton.insetsLeft
     }
     
     func setupConstraints() {
-        [titleLabel, backButton, alphabetSortButton, birthdaySortButton].forEach { addSubview($0) }
+        [titleLabel, backButton, buttonStack].forEach { addSubview($0) }
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -61,22 +55,34 @@ private extension SortView {
             backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.BackButton.leading)
         ])
         
-        alphabetSortButton.translatesAutoresizingMaskIntoConstraints = false
+        [alphabetSortButton, birthdaySortButton].forEach { buttonStack.addArrangedSubview($0) }
+        
+        buttonStack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            alphabetSortButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor,
-                                                    constant: Constants.AlphabetSortButton.top),
-            alphabetSortButton.leadingAnchor.constraint(equalTo: leadingAnchor,
-                                                        constant: Constants.AlphabetSortButton.leading),
-            alphabetSortButton.widthAnchor.constraint(equalToConstant: Constants.AlphabetSortButton.width)
+            buttonStack.topAnchor.constraint(equalTo: titleLabel.bottomAnchor,
+                                             constant: Constants.AlphabetSortButton.top),
+            buttonStack.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,
+                                                 constant: Constants.AlphabetSortButton.leading),
+            buttonStack.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,
+                                                 constant: Constants.AlphabetSortButton.trailing)
         ])
         
-        birthdaySortButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            birthdaySortButton.topAnchor.constraint(equalTo: alphabetSortButton.bottomAnchor,
-                                                    constant: Constants.BirthdaySortButton.top),
-            birthdaySortButton.leadingAnchor.constraint(equalTo: alphabetSortButton.leadingAnchor),
-            birthdaySortButton.widthAnchor.constraint(equalToConstant: Constants.BirthdaySortButton.width)
-        ])
+//                alphabetSortButton.translatesAutoresizingMaskIntoConstraints = false
+//                NSLayoutConstraint.activate([
+        //            alphabetSortButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor,
+        //                                                    constant: Constants.AlphabetSortButton.top),
+        //            alphabetSortButton.leadingAnchor.constraint(equalTo: leadingAnchor,
+        //                                                        constant: Constants.AlphabetSortButton.leading),
+        //            alphabetSortButton.widthAnchor.constraint(equalToConstant: Constants.AlphabetSortButton.width)
+        //        ])
+        //
+        //        birthdaySortButton.translatesAutoresizingMaskIntoConstraints = false
+        //        NSLayoutConstraint.activate([
+        //            birthdaySortButton.topAnchor.constraint(equalTo: alphabetSortButton.bottomAnchor,
+        //                                                    constant: Constants.BirthdaySortButton.top),
+        //            birthdaySortButton.leadingAnchor.constraint(equalTo: alphabetSortButton.leadingAnchor),
+        //            birthdaySortButton.widthAnchor.constraint(equalToConstant: Constants.BirthdaySortButton.width)
+        //        ])
     }
 }
 
@@ -95,15 +101,11 @@ private enum Constants {
         static let leading: CGFloat = 33.5
     }
     
-    enum SortButton {
-        static let font = R.Fonts.interMedium(with: 16)
-        static let insetsLeft: CGFloat = 14
-    }
-    
     enum AlphabetSortButton {
         static let text = "По алфавиту"
         static let top: CGFloat = 41.5
         static let leading: CGFloat = 26
+        static let trailing: CGFloat = 26
         static let width: CGFloat = 140
     }
     
