81b2e92d567ffbad6e2033f3fa019b0b8d63427e
NikitaRekaev <nvvvvk@gmail.com>
2022-11-08 12:41:58 +0600

    Deleted unnecessary comments for BaseViewController

--- Coder/Sources/Supporting/Base/BaseViewController.swift
+++ Coder/Sources/Supporting/Base/BaseViewController.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 class BaseViewController<View: UIView>: UIViewController {
-    // swiftlint:disable force_cast
+    
     var mainView: View { view as! View }
-    // swiftlint:enable force_cast
+    
     override func loadView() {
             view = View()
     }
