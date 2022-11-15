f00986e8538aa7ddc20c44e93ec8f64b5ef96f86
NikitaRekaev <nvvvvk@gmail.com>
2022-11-15 16:59:26 +0600

    Created UIView+AddView

---
+++ Coder/Sources/Supporting/Extention/UIKit/UIView+AddView.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIView {
+    
+    func addView(_ view: UIView) {
+          self.addSubview(view)
+          view.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
