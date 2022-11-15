1c9f902ef3151389fe61c2c9159a160f19f50a8c
NikitaRekaev <nvvvvk@gmail.com>
2022-11-15 17:08:54 +0600

    Refactored layout for TopTabsCollectionViewCell

--- Coder/Sources/Modules/MainScreen/Views/Cells/TopTabsCollectionViewCell.swift
+++ Coder/Sources/Modules/MainScreen/Views/Cells/TopTabsCollectionViewCell.swift
@@ -28,29 +28,25 @@ final class TopTabsCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         [label, bottomBorderView].forEach { contentView.addSubview($0) }
+        [contentView, label, bottomBorderView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
         
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor),
-            label.heightAnchor.constraint(equalTo: contentView.heightAnchor),
-            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-        ])
-        
-        bottomBorderView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            bottomBorderView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            bottomBorderView.leadingAnchor.constraint(equalTo: label.leadingAnchor),
-            bottomBorderView.trailingAnchor.constraint(equalTo: label.trailingAnchor),
-            bottomBorderView.heightAnchor.constraint(equalToConstant: Constants.borderHeight)
-        ])
-        
-        contentView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             contentView.heightAnchor.constraint(equalToConstant: Constants.Content.height),
             contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.Content.leading),
             contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            contentView.widthAnchor.constraint(equalTo: widthAnchor, constant: Constants.Content.width)
+            contentView.widthAnchor.constraint(equalTo: widthAnchor, constant: Constants.Content.width),
+            
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            label.heightAnchor.constraint(equalTo: contentView.heightAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            bottomBorderView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            bottomBorderView.leadingAnchor.constraint(equalTo: label.leadingAnchor),
+            bottomBorderView.trailingAnchor.constraint(equalTo: label.trailingAnchor),
+            bottomBorderView.heightAnchor.constraint(equalToConstant: Constants.borderHeight)
         ])
     }
 }
